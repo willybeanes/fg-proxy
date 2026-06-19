d5fe6b2513e5e5c57fbd3d052a236d4bec896531
Will Harris <will@Wills-MBP.lan>
2026-06-18 23:05:48 -0500

    Add /api/search/player to allowed proxy paths

--- api/fg-gamelog.js
+++ api/fg-gamelog.js
@@ -7,6 +7,7 @@ const ALLOWED_PATHS = [
   '/api/players/game-log',
   '/api/players/playerSearch',
   '/api/leaders/leaders',
+  '/api/search/player',
 ]
 
 export default async function handler(req, res) {
